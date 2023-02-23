84c88d65994b5584cacfc244c695950516d6e367
Nontakran <nontakran_mo@kkumail.com>
2023-02-23 21:39:31 +0700

    first commit

--- components/posts/Header.vue
+++ components/posts/Header.vue
@@ -12,9 +12,9 @@
                     <b-nav-item href="/">Home</b-nav-item>
                     <div class="">
                         <b-nav-item-dropdown text="360 virtual tour" right>
-                            <b-dropdown-item href="/Virtual1.html">1st floor</b-dropdown-item>
-                            <b-dropdown-item href="/Virtual2.html">2st floor</b-dropdown-item>
-                            <b-dropdown-item href="/Virtual3.html">3st floor</b-dropdown-item>
+                            <b-dropdown-item href="/T copy.html">1st floor</b-dropdown-item>
+                            <b-dropdown-item href="/T copy.html">2st floor</b-dropdown-item>
+                            <b-dropdown-item href="/T copy 3.html">3st floor</b-dropdown-item>
                         </b-nav-item-dropdown>
                     </div>
                     <!-- <b-nav-item href="/Virtual.html">360 virtual tour</b-nav-item> -->
