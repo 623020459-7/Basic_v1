7b82ac16ca6d4bc8273d655654ac398cd2ee083c
BASIC_V1_FEATURE_FIRST <Kanokphon_ka@kkumail.com>
2023-03-27 17:54:49 +0700

    Restore AdminForm

--- pages/admin/index2.vue
+++ pages/admin/index2.vue
@@ -1,16 +1,30 @@
 <template>
     <div>
         <b-container fluid="md">
-            <AdminForm />
+            <HeaderAdmin />
+            <AdminForm @reset="onReset" />
         </b-container>
     </div>
 </template>
 <script>
+import HeaderAdmin from '../../components/posts/HeaderAdmin.vue';
 import AdminForm from '../../components/admin/AdminForm.vue'
 export default {
     layout: "coreLayout",
     components: {
+        HeaderAdmin,
         AdminForm,
-    }
+
+    },
+    methods: {
+        onReset(deleteData) {
+            axios.delete("http://localhost:8000/api/deletePerformance/" + performance_id, deleteData)
+                .then(res => {
+                    console.log(res);
+                    // this.route.push("/admin/index2")
+                })
+            console.log("Result", deleteData);
+        }
+    },
 }
 </script>
