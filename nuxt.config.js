3328f7a9e47f108826c765dd8dcd0995487eb783
BASIC_V1_FEATURE_FIRST <Kanokphon_ka@kkumail.com>
2023-04-11 00:30:55 +0700

    host web on port 80

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,10 @@
 export default {
+  // IP and PORT
+  server: {
+    // host: '0',
+    port: 80
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'Basic',
